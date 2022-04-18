1de2db0f2938d283e63f9602f9fc039a7ba5bd03
EltsinDS <ds.eltsin@gmail.com>
2022-04-18 14:06:12 +0300

    Finish redux and table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -15,7 +15,7 @@ class Dom {
     }
 
     text(text){
-        if(typeof text === 'string'){
+        if(typeof text !== 'undefined'){
             this.$el.textContent = text
             return this
         }
@@ -72,10 +72,18 @@ class Dom {
     css(styles = {}){
         Object
             .keys(styles)
-            .forEach( key => {
+            .forEach(key => {
                 this.$el.style[key] = styles[key]
             })
     }
+
+    getStyles(styles = []){
+        return styles.reduce((res, s)=> {
+            res[s] = this.$el.style[s]
+            return res
+        }, {})
+    }
+
     id(parse){
         if (parse){
             const parsed = this.id().split(':')
@@ -90,6 +98,14 @@ class Dom {
     focus(){
         this.$el.focus()
         return this
+    }
+
+    attr(name,value){
+        if (value){
+            this.$el.setAttribute(name, value)
+            return this
+        }
+        return this.$el.getAttribute(name)
     }
 
     addClass(className){
